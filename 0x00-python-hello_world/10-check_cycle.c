04842b60dd8135b19067afb2dea1237f632544bb
Mapu456 <mpaula456@hotmail.com>
2021-09-06 17:23:52 -0500

    pyhton

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * check_cycle - checks a cycle
+ * @list: linked list
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+
+}
