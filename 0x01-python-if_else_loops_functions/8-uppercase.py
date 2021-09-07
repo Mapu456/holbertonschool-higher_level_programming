55c5fdf0b6279643724cd5b4d9d24ce8591d832e
Mapu456 <mpaula456@hotmail.com>
2021-09-07 15:03:03 -0500

    python

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
 def uppercase(str):
-    upperca = str.upper()
-    print("{}".format(upperca))
+    for letter in str:
+        if letter == " ":
+            letter = '~'
+        if letter == '9':
+            letter = '{'
+        if letter == '8':
+            letter = '}'
+        number = ord(letter)
+        if number < 96:
+            number = number + 32
+        if 96 < number < 127:
+            number = number - 32
+            if number == 94:
+                number = 32
+            if number == 91:
+                number = 57
+            if number == 93:
+                number = 56
+            print("{:c}".format(number), end="")
+    print(" ")
