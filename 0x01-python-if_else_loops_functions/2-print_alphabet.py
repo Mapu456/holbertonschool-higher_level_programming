49d2d79dd069c970d624ebe80ac1120ba593ea3b
Mapu456 <mpaula456@hotmail.com>
2021-09-07 13:13:42 -0500

    python

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-for c in range(97, 123):
-    print("{:c}".format(c), end=" ")
+def lowercaseAlphabets():
+    for c in range(97, 123):
+        print("{:c}".format(c), end="")
+
+
+lowercaseAlphabets()
--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,7 +2,6 @@
 for c in range(0, 100):
     if c == 99:
         print("{:d}".format(c))
-    elif c < 10:
-        print("0{:d}".format(c), end=", ")
     else:
-        print("{:d}".format(c), end=", ")
+        print("{:02d}".format(c), end=", ")
+
