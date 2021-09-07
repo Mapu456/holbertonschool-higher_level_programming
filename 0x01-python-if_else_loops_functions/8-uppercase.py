8f8bd6ae3f1a61380e258a5ce82e90da51707384
Mapu456 <mpaula456@hotmail.com>
2021-09-07 15:23:36 -0500

    python

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+def uppercase(str):
+    for letter in str:
+        number = ord(letter)
+        if 96 < number < 123:
+            number = number - 32
+        print("{:c}".format(number), end="")
+    print(" ")
