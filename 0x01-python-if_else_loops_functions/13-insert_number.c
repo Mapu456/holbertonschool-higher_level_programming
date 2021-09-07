0827ba9fb60010f07beb1d7c818a53c05ef57ad7
Mapu456 <mpaula456@hotmail.com>
2021-09-07 16:45:26 -0500

    python

---
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * insert_node - inserts a node
+ * @head: head
+ * @number: int to add in the node
+ * Return: the address of the new node, or NULL if it failede
+ */
+listint_t *insert_node(listint_t **head, int number)
+{
+
+}
