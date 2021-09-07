ff8c2987bf180abf07a04b1c010e18a5d0432b6c
Mapu456 <mpaula456@hotmail.com>
2021-09-07 15:46:37 -0500

    python

---
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_last_digit(number):
+	if number < 0:
+		number = number * -1
+	for last_digit in str(number):
+		last3 = int(last_digit[-1])
+	print("{:d}".format(last3))
