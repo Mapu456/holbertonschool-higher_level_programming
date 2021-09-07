bbc25be507ff6c832f3967caa9c3f66446648848
Mapu456 <mpaula456@hotmail.com>
2021-09-06 20:27:09 -0500

    fixing env

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -8,5 +8,18 @@
  */
 int check_cycle(listint_t *list)
 {
+	listint_t *before = list;
+	listint_t *after = list;
 
+	while (after->next != NULL && after != NULL)
+	{
+		before = before->next;
+		after = after->next->next;
+
+		if (before == after)
+		{
+			return (1);
+		}
+	}
+	return (0);
 }
