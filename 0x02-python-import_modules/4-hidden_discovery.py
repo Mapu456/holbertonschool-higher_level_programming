48bb050d0f6f4639985e14c63ef31245ca1c30c1
Mapu456 <mpaula456@hotmail.com>
2021-09-08 16:28:11 -0500

    modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -2,5 +2,5 @@
 if __name__ == "__main__":
     import hidden_4
     for arguments in dir(hidden_4):
-        if arguments[0:2] != '__':
+        if arguments[0:1] != '_':
             print(arguments)
