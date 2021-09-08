67e3aef979d24ee5ea3c72836fe57f602a95233a
Mapu456 <mpaula456@hotmail.com>
2021-09-08 14:38:41 -0500

    modules

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,11 @@ if __name__ == "__main__":
     import sys
     long = len(sys.argv) - 1
     if long == 1:
-        print("{:d} argument:".format(long))
+        print("{:d} argument: ".format(long))
     elif long > 1:
-        print("{:d} arguments:".format(long))
+        print("{:d} arguments: ".format(long))
     else:
-        print("{:d} arguments.".format(long))
+        print("{:d} arguments. ".format(long))
     arg0 = sys.argv[1:]
     count = 0
     for arguments in arg0:
