3c747deebff75013b33e53a527af678024beec5e
Mapu456 <mpaula456@hotmail.com>
2021-09-07 20:34:22 -0500

    python

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,7 +2,7 @@
 for c in range(0, 10):
     for i in range(0, 10):
         if c == 8 and i == 9:
-            print("{:d}{:d}".format(c, i))
+            print("{:d}{:d}".format(c, i), end="\n")
         elif c == 1 and i == 0:
             continue
         elif c < i:
