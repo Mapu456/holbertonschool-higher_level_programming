1b3b17d9662686ea5fbae8d3645d7b428cc78a49
Mapu456 <mpaula456@hotmail.com>
2021-09-08 16:56:18 -0500

    modules

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
-def uppercase(str_data):
-    return ''.join([chr(ord(char) - 32) for char in str_data if ord(char) >= 65])
+if __name__ == "__main__":
+    str = ""
 
+    def uppercase(str_data):
+        return (str.join([chr(ord(char) - 32) for char in str_data if ord(char) >= 65]))
 
-print(uppercase("abcdefghijlmnopqrstuvwxyz"))
+    print(uppercase("abcdefghijlmnopqrstuvwxyz"))
