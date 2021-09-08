5f7c457a039e872ab87715779c154bdf4f5258b2
Mapu456 <mpaula456@hotmail.com>
2021-09-08 16:46:16 -0500

    modules

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,4 @@ if __name__ == "__main__":
     import hidden_4
     for arguments in dir(hidden_4):
         if arguments[0:2] != '__':
-            if arguments[0:12] == "print_hidden":
-                print("print_holberton")
-            else:
-            	print("{:s}".format(arguments))
+            print("{:s}".format(arguments))
