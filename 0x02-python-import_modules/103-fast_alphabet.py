e8d01bcabfd96ba2290d7c9d9a89e0d24cc8b73e
Mapu456 <mpaula456@hotmail.com>
2021-09-08 17:09:00 -0500

    modules

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     str = ''
-    print(str.join(chr(ord(char) - 32)))
+    print((str.join([chr(ord(char) - 32) for char in "abcdefghijlmnopqrstuvwxyz" if ord(char) >= 65])))
