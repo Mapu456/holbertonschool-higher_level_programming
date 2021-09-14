3cb0f0878514276a72597b820699af2eb4271a63
Mapu456 <mpaula456@hotmail.com>
2021-09-14 15:46:00 -0500

    diretions

---
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def multiply_by_2(a_dictionary):
+    new_dictionary = {}
+    new_dictionary = a_dictionary.copy()
+    for keys, values in new_dictionary.items():
+        new_dictionary[keys] = values * 2
+    return new_dictionary
