d1084aca7e83f5d6da808861401240d5abefe893
Mapu456 <mpaula456@hotmail.com>
2021-09-14 15:17:56 -0500

    diretions

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def number_keys(a_dictionary):
-    for key in sorted(a_dictionary):
-        return key
+def print_sorted_dictionary(a_dictionary):
+    order = sorted(a_dictionary.items())
+    for i, x in order:
+        print("{:s}: {}".format(i, x))
