f85a393bc5d77f1a8149732eb65057de571eb7c6
Mapu456 <mpaula456@hotmail.com>
2021-09-13 19:45:09 -0500

    python_list

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-	lon = len(tuple_a)
-	lon1 = len(tuple_b)
-	if lon < 2 or lon1 < 2:
-		print("0, 0")
-	return(tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
+    tuple3 = (0, 0)
+    tuple1 = tuple_a + tuple3
+    tuple2 = tuple_b + tuple3
+    return (tuple1[0] + tuple2[0], tuple1[1] + tuple2[1])
