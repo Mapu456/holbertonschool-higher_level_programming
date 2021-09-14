6542f7d42b030629a1973d55b2e281ca9920d605
Mapu456 <mpaula456@hotmail.com>
2021-09-14 17:28:46 -0500

    diretions

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
-    for key in list(a_dictionary.keys()):
+    if a_dictionary == None:
+        return None
+    for key in a_dictionary.keys():
         if a_dictionary[key] == value:
-            del a_dictionary.key
+            del a_dictionary[key]
     return a_dictionary
