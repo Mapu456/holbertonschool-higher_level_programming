a2470f292b79d7ce395d44e0648e3e1b2cef5d76
Mapu456 <mpaula456@hotmail.com>
2021-09-14 12:19:31 -0500

    diretions

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -2,4 +2,5 @@
 def square_matrix_simple(matrix=[]):
     for initial in matrix:
         for i in initial:
-            return list(map(lambda initial: list(map(lambda i: i ** 2, initial)), matrix))
+            return list(map(lambda initial: list(map(lambda i: i ** 2, initial)),
+			matrix))
