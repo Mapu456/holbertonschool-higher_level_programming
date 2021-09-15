6b71b8d8106006ccfcd072fd63b89720dcef8402
Mapu456 <mpaula456@hotmail.com>
2021-09-14 19:59:20 -0500

    python_list

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-	if not my_list:
-		return 0
-	return list(map(lambda initial: list(map(lambda i, c: ((i * c)/c), initial)),
-	my_list))
+    if not my_list:
+        return 0
+    mult = 0
+    sum2 = 0
+    for i in my_list:
+        sum2 += i[1]
+        mult += i[0] * i[1]
+        total = mult/sum2
+    return total
