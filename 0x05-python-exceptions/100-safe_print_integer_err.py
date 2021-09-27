a1bd048abf374457b9bf38ddb5c2381d12a58af3
Mapu456 <mpaula456@hotmail.com>
2021-09-27 18:36:59 -0500

    exceptions

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,7 @@ def safe_print_integer(value):
         elif value >= 0:
             print("{:d}".format(value))
             return True
-    except Exception:
+        import sys
+    except Exception as ex:
+        print("Exception: {}".format(ex), file=sys.stderr)
         return False
