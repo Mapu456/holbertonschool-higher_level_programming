5a3ccf92b89423e2666fe3a0ce1e1400db702dfb
Mapu456 <mpaula456@hotmail.com>
2021-09-27 12:10:54 -0500

    exceptions

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-	try:
-		if value < 0:
-			print("{:d}".format(value))
-			return True
-		elif value >= 0:
-			print("{:d}".format(value))
-			return True
-	except TypeError:
-		return False
+    try:
+        if value < 0:
+            print("{:d}".format(value))
+            return True
+        elif value >= 0:
+            print("{:d}".format(value))
+            return True
+    except (TypeError, ValueError):
+        return False
