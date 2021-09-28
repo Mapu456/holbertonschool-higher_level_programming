27e626fb7a57975ce87f6fc885796123fc3020bd
Mapu456 <mpaula456@hotmail.com>
2021-09-28 12:06:26 -0500

    OPP

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -5,5 +5,8 @@
 class Square():
     """Write an empty class Square that defines a square"""
 
+    def __init__(self, size):
+        self.__size = size
 
-my_square = Square()
+
+my_square = Square(3)
