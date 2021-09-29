9634ca67d3da392a89510f0cced4238ef668850d
Mapu456 <mpaula456@hotmail.com>
2021-09-29 15:32:06 -0500

    opp

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""square class"""
+
+
+class Square():
+    """Write an empty class Square that defines a square"""
+
+    def __init__(self, size=0):
+        self.__size = size
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+    def area(self):
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """Get size value"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        self.__size = value
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+
+    def my_print(self):
+        if self.__size == 0:
+            print(" ")
+        else:
+            for i in range(self.__size):
+                print('#' * self.__size)
+
+
+my_square = Square()
