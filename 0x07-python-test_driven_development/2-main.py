595484065e544d8f1b6b195789f71f2d0cc67cfd
Mapu456 <mpaula456@hotmail.com>
2021-10-05 11:57:32 -0500

    test

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [[-1.0, 2.55, -3],[-4, 5.5, -6.3]]
+matrix = [[1.0, 2.55, -3.2], [-4, 5.5, 6.3]]
 print(matrix_divided(matrix, -3))
 print(matrix)
