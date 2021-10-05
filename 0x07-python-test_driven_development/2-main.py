db6423776483d832a4e3e57ca3cc007f62762a3e
Mapu456 <mpaula456@hotmail.com>
2021-10-05 11:10:34 -0500

    test

--- 0x07-python-test_driven_development/2-main.py
+++ 0x07-python-test_driven_development/2-main.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 matrix_divided = __import__('2-matrix_divided').matrix_divided
 
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6]
-]
-print(matrix_divided(matrix, 3))
+matrix = [[-1.0, 2.55, -3],[-4, 5.5, -6.3]]
+print(matrix_divided(matrix, -3))
 print(matrix)
