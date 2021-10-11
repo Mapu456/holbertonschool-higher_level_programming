bab5b836657fe1c0cbe15eb1b857b7b77995ed7d
Mapu456 <mpaula456@hotmail.com>
2021-10-11 18:37:50 -0500

    inheritanceand test

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-"""Module for lookup"""
+"""Module for 'is_same_class' method"""
 
 
 def is_same_class(obj, a_class):
+    """verify the type of obj
+    Arguments:
+        obj: object to check
+        a_class: class to be checked
+    Return:
+        True or false
+    """
     return (type(obj) is a_class)
