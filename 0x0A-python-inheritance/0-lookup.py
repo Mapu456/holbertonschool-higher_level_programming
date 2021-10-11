cfd3a9e1ca0098eafffb3f7923e53e5cde79658b
Mapu456 <mpaula456@hotmail.com>
2021-10-11 18:20:04 -0500

    inheritanceand test

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function for lookup"""
+
+
+def lookup(obj):
+    """
+    Function that give me  a list of  attributes or methods
+    """
+    return (dir(obj))
---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for lookup"""
+
+class MyList(list):
+    """Myclass of type list
+    """
+    def print_sorted(self):
+        """Prints the list sorted
+        """
+        print(sorted(self))
