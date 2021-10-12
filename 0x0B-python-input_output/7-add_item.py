15b9bff307ecdae4200485549981dce5bca54495
Mapu456 <mpaula456@hotmail.com>
2021-10-12 18:39:29 -0500

    read and write

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -8,4 +8,4 @@ import json
 def save_to_json_file(my_obj, filename):
     """Creates an object from a JSON file"""
     with open(filename, 'w', encoding='utf-8') as f:
-        return (json.dumps(my_obj))
+        json.dump(my_obj, f)
