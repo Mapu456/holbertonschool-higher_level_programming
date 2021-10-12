6b8b273890d871da51a97b385fc09f647b71ef16
Mapu456 <mpaula456@hotmail.com>
2021-10-11 21:19:47 -0500

    inheritanceand test

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""Module BaseGeometry class"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Empty class BaseGeometry"""
+
+    def __init__(self, width, height):
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        """this Method print area of rectangle"""
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """this Method return Rectangle"""
+        return ("[Rectangle] {:d}/{:d}".format(self.__width, self.__height))
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """this Method print area of rectangle"""
+        return (self.__size * self.__size)
+
+    def __str__(self):
+        """this Method return Rectangle"""
+        return ("[Rectangle] {:d}/{:d}".format(self.__size, self.__size))
