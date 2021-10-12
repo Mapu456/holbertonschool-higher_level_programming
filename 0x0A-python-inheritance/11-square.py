0d42c975ebfb88b937f8b5f63ed5779108bd340b
Mapu456 <mpaula456@hotmail.com>
2021-10-11 21:37:50 -0500

    inheritanceand test

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module BaseGeometry class"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        """this Method print area of rectangle"""
+        return (Rectangle.area(self))
