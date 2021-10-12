11caaecc1d98e592be49cb07e7d00b1d47423fe7
Mapu456 <mpaula456@hotmail.com>
2021-10-12 10:14:54 -0500

    read and write

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Module for read the file with read_file method"""
+
+
+def append_write(filename="", text=""):
+    with open('filename', 'a', encoding='utf-8') as f:
+        read_data = f.write(text)
+        return(read_data)
