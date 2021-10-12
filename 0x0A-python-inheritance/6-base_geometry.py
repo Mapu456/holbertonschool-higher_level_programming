46853b29c072de0bb360835f31c876c362cdd315
Mapu456 <mpaula456@hotmail.com>
2021-10-11 19:44:41 -0500

    inheritanceand test

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module BaseGeometry class"""
+
+
+class BaseGeometry:
+    """Empty class BaseGeometry"""
+
+    def area(self):
+        raise Exception("area() is not implemented")
