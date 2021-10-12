53a8262f268454d325f3226f48cd54951c97bb9a
Mapu456 <mpaula456@hotmail.com>
2021-10-12 17:09:26 -0500

    read and write

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,8 +5,8 @@
 def write_file(filename="", text=""):
     """Read and print a text file
     Args:
-        filename: Text file. Defaults to "".
+        filename: Text file.""
     """
-    with open('filename', 'w', encoding='utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
     	read_data = f.write(text)
     	return(read_data)
