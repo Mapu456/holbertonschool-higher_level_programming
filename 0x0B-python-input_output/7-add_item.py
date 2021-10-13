351fbead51f5d0488ff51ae6e92177cd5a0a252c
Mapu456 <mpaula456@hotmail.com>
2021-10-12 20:31:12 -0500

    inheritanceand test

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -3,17 +3,15 @@
 
 
 from sys import argv
-list = []
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-while("add_item.json"):
-    a = True
-    break
-if a == True:
-    list = load_from_json_file("add_item.json")
-for i in argv[1:]:
-    list.append(i)
+try:
+    elems_list = load_from_json_file("add_item.json")
+except:
+    elems_list = []
 
-save_to_json_file(list, "add_item.json")
+for args in (argv[1:]):
+    elems_list.append(args)
+    save_to_json_file(elems_list, "add_item.json")
