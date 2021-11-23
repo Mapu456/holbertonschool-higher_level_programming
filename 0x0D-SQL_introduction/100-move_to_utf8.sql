40c91fbe4f7bdfef96072f7e93813496dacc7bba
Mapu456 <mpaula456@hotmail.com>
2021-11-23 15:25:35 -0500

    fix point 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,14 +1,14 @@
 -- script that converts hbtn_0c_0 database to UTF8
 ALTER DATABASE
     hbtn_0c_0
-    CHARACTER SET = utf8mb4
+    CHARACTER SET = utf8
     COLLATE = utf8mb4_unicode_ci;
 ALTER TABLE
-    hbtn_0c_0.first_table
+    first_table
     CONVERT TO CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
 ALTER TABLE
-    hbtn_0c_0.first_table
+    first_table
     CHANGE name name
     VARCHAR(191)
     CHARACTER SET utf8mb4
