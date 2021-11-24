f1b31b9a9ad18a6900d7f34dba427e188ecef250
Mapu456 <mpaula456@hotmail.com>
2021-11-24 16:20:31 -0500

    point 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,11 @@
+-- script that creates the database hbtn_0d_usa and the table cities
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities
+(
+	id INT UNIQUE AUTO_INCREMENT,
+	state_id INT NOT NULL,
+	name VARCHAR(256) NOT NULL,
+	PRIMARY KEY(id),
+	FOREING KEY(state_id)
+);
