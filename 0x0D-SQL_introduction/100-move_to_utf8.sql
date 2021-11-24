694acfcb05d3685312bae30fa450f11b510d5df7
Mapu456 <mpaula456@hotmail.com>
2021-11-24 11:46:14 -0500

    fix point 17

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -8,10 +8,3 @@ ALTER TABLE
     hbtn_0c_0.first_table
     CONVERT TO CHARACTER SET utf8mb4
     COLLATE utf8mb4_unicode_ci;
-
-ALTER TABLE
-    hbtn_0c_0.first_table
-    CHANGE name name
-    varchar(256)
-    CHARACTER SET utf8mb4
-    COLLATE utf8mb4_unicode_ci;
