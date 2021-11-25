2e105f649cc3c9ddc5d469c1a2bf9e26d843d5c9
Mapu456 <mpaula456@hotmail.com>
2021-11-25 11:05:46 -0500

    point 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,6 +3,6 @@ SELECT
 	tv_genres.name as genre, COUNT(*) AS number_of_shows
 FROM
 	tv_genres
-INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_genres ON tv_genres.id=tv_show_genres.genre_id
 GROUP BY tv_show_genres.genre_id
-ORDER BY number_of_shows desc;
+ORDER BY number_of_shows DESC;
