f450fb8e6874a1f616a7b2037d8f768b5de9b261
Mapu456 <mpaula456@hotmail.com>
2021-11-25 19:21:46 -0500

    point 100

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,12 +1,9 @@
 -- script that uses the hbtn_0d_tvshows database to lists all genres of the show Dexter.
-select tv_genres.name
+select tv_genres.name FROM tv_genres WHERE name NOT IN (SELECT tv_genres.name
 FROM tv_genres
-WHERE name NOT IN
-	(SELECT tv_genres.name
-	FROM tv_genres
-	INNER JOIN tv_show_genres
-	ON tv_genres.id = tv_show_genres.genre_id
-	INNER JOIN tv_shows
-	ON tv_show_genres.show_id = tv_shows.id
-	WHERE tv_shows.title = "Dexter"
-	ORDER BY tv_genres.name);
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_show_genres.show_id = tv_shows.id
+WHERE tv_shows.title = "Dexter"
+ORDER BY tv_genres.name);
