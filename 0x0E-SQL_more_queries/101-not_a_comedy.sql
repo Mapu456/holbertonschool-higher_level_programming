c74ccd0260006343336ab47b71cbc8c0a51adfe6
Mapu456 <mpaula456@hotmail.com>
2021-11-25 19:32:42 -0500

    point 101

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,9 @@
+-- script that uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+select tv_shows.title from tv_shows where title not in (SELECT tv_shows.title
+FROM tv_shows
+INNER JOIN tv_show_genres
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_genres
+ON tv_show_genres.genre_id = tv_genres.id
+WHERE tv_genres.name = "Comedy")
+ORDER BY tv_shows.title;
