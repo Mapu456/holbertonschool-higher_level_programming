dea4f5b4505c6f12178963c8263e7174b4755192
Mapu456 <mpaula456@hotmail.com>
2021-12-14 16:16:48 -0500

    point 9

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,26 +1,29 @@
 #!/usr/bin/node
 
 class Rectangle {
-	constructor(w, h) {
-		if (w * h> 0) {
-			this.width = w;
-			this.height = h;
-		}
-	}
-	print() {
-		for (let i = 0; i < this.height; i++) {
-			console.log('X'.repeat(this.width))
-		}
-	}
-	rotate() {
-		const first_with = this.width;
-		this.width = this.height;
-		this.height = first_with;
-	}
-	double() {
-		this.width *= 2;
-		this.height *= 2;
-	}
+  constructor (w, h) {
+    if (w * h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () {
+    const firstWith = this.width;
+    this.width = this.height;
+    this.height = firstWith;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
 }
 
 module.exports = Rectangle;
