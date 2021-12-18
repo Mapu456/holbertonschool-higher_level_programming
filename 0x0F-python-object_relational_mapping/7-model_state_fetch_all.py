45018afc35ede902e2d83e579beb08a3db2830b9
Mapu456 <mpaula456@hotmail.com>
2021-12-18 12:45:42 -0500

    point 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,1 +1,26 @@
-hhhhhhhhhhhhh
+#!/usr/bin/python3
+"""Function to print states in asc order"""
+
+
+from sys import argv
+import MySQLdb
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    cur = db.cursor()
+    cur.execute("select * from states")
+    row = cur.fetchall()
+    lon = len(row)
+    count = 0
+    for i in row:
+        print("{}: {}".format(i[0], i[1]))
+
+    cur.close()
+    db.close()
