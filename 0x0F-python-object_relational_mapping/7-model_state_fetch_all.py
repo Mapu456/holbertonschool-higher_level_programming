8d06117d83fc14644925513dbe936d1b91625f3e
Mapu456 <mpaula456@hotmail.com>
2021-12-19 11:31:38 -0500

    point 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,20 +1,18 @@
 #!/usr/bin/python3
-"""Function to print states in asc order"""
+""" Lists all State objects from the database hbtn_0e_6_usa
+"""
 
-
-from sys import argv
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
+from sys import argv
 
-if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        argv[1], argv[2], argv[3]), pool_pre_ping=True)
-    Session = sessionmaker()
-    session = Session(bind=engine)
+if __name__ == '__main__':
+    engine = create_engine(
+        'mysql+mysqldb://{}:{}@localhost/{}'.format(argv[1], argv[2], argv[3]))
+    Base.metadata.create_all(engine)
 
-    query = session.query(State).orderby(State.id).all
-
-    if query:
-        print("{}: {}".format(query.id, query.name))
+    session = Session(engine)
+    for state in session.query(State).order_by(State.id).all():
+        print("{}: {}".format(state.id, state.name))
     session.close()
