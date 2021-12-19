54fdcf494010de4c329f68594936c67c68903e34
Mapu456 <mpaula456@hotmail.com>
2021-12-19 11:25:09 -0500

    point 7

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -10,14 +10,10 @@ from sqlalchemy import (create_engine)
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
-
-    Base.metadata.create_all(engine)
     Session = sessionmaker()
     session = Session(bind=engine)
 
-    query = session.query(State).first()
+    query = session.query(State).orderby(State.id)
 
     if query:
         print("{}: {}".format(query.id, query.name))
-    else:
-        print("Nothing")
--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -1,7 +1,7 @@
-
 #!/usr/bin/python3
-"""Start link class to table in database
-"""
+"""Function to print states in asc order"""
+
+
 from sys import argv
 from model_state import Base, State
 from sqlalchemy.orm import sessionmaker
@@ -10,10 +10,14 @@ from sqlalchemy import (create_engine)
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
+
+    Base.metadata.create_all(engine)
     Session = sessionmaker()
     session = Session(bind=engine)
 
-    query = session.query(State).order_by(State.id)
+    query = session.query(State).where(State.name == 'California')
 
-    for item in query:
-        print("{}: {}".format(item.id, item.name))
+    if query:
+        print("{}: {}".format(query.id, query.name))
+    else:
+        print("Nothing")
