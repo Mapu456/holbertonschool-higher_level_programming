33674d1803dd816779b1b45a5e4595c913716b1d
Mapu456 <mpaula456@hotmail.com>
2022-01-10 12:11:14 -0500

    point 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,6 @@ from sys import argv
 if __name__ == "__main__":
     request = requests.get(argv[1])
     if request.status_code >= 400:
-        print('Error code: {}'.format(request.code))
+        print('Error code: {}'.format(request.status_code))
     else:
         print(request.text)
