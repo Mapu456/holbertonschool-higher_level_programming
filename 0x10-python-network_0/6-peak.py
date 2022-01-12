a659aed906cb3c22d17e9ded3310e500b9b90005
Mapu456 <mpaula456@hotmail.com>
2022-01-12 02:30:48 +0000

    confi load balancer and install

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
+def findPeak(list_of_integers, left=None, right=None):
+    n = len(list_of_integers)
+    if left is None and right is None:
+        left, right = 0, len(list_of_integers) - 1
+    mid = (left + right) // 2
+    if ((mid == 0 or list_of_integers[mid - 1] <= list_of_integers[mid]) and (mid == n - 1 or list_of_integers[mid + 1] <= list_of_integers[mid])):
+        return mid
+    if mid - 1 >= 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
+        return findPeak(list_of_integers, left, mid - 1)
+    return findPeak(list_of_integers, mid + 1, right)
+ 
+ 
 def find_peak(list_of_integers):
-    if list_of_integers:
-        return max(list_of_integers)
-    else:
+    if list_of_integers == []:
         return "None"
+    index = findPeak(list_of_integers)
+    return list_of_integers[index]
